5aba550192189ba48c041e965738fc72fcc5e577
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2019-05-20 14:52:30 +0100

    remove identifier temp table generation and point to index

--- ngis-mq-report.r
+++ ngis-mq-report.r
@@ -10,17 +10,9 @@ library(rmarkdown)
 
 #-- connect to results db
 drv <- dbDriver("PostgreSQL")
-p <- getprofile(
-				"local_postgres_con"
-				  )
+p <- getprofile("indx_con")
 res_db_con <- dbConnect(drv,
-             dbname = "ngis_mq_results",
-             host     = p$host,
-             port     = p$port,
-             user     = p$user,
-             password = p$password)
-gr_db_con <- dbConnect(drv,
-             dbname = "testing",
+             dbname = "metrics",
              host     = p$host,
              port     = p$port,
              user     = p$user,
@@ -28,24 +20,14 @@ gr_db_con <- dbConnect(drv,
 
 
 #-- get latest data from results db
-md <- dbGetQuery(res_db_con, "select * from vw_latest_metric_results;")
-tr <- dbGetQuery(res_db_con, "select * from vw_latest_test_results;")
-
-#-- identifier table, provides linkage of the different identifier_types to GLHs
-id <- dbGetQuery(gr_db_con, paste(readLines("identifier-table.sql"), collapse = " "))
-
-#-- merge in id table on both identifier_uid and identifier_type
-md <- merge(md, id, by = c("identifier_uid", "identifier_type"), all.x = T)
-tr <- merge(tr, id, by = c("identifier_uid", "identifier_type"), all.x = T)
-
-#-- get rule fails per GLH
-test_fails <- split(tr[!tr$test_result,], tr$ordering_entity_uid[!tr$test_result])
+md <- dbGetQuery(res_db_con, "select * from ngis_mq_results.vw_latest_metric_results;")
+tr <- dbGetQuery(res_db_con, "select * from ngis_mq_results.vw_latest_test_results;")
 
 #-- render the metrics report for all GLHs
 render("ngis-mq-report-all-glhs.rmd", output_format = "word_document")
 
 #-- render individual GLH reports
-for(glh in unique(md$ordering_entity_uid)){
+for(glh in unique(md$glh)){
 	render("ngis-mq-report-per-glh.rmd",
 		   output_file = paste0("ngis-mq-report-", glh, ".docx"),
 		   output_format = "word_document")
