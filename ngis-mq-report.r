7b2faf5df261e8ecf9aecc37108cbdcfb2d2b37e
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2019-07-02 12:47:35 +0100

    various changes including adding ddf data and extra columns

--- ngis-mq-report.r
+++ ngis-mq-report.r
@@ -1,3 +1,4 @@
+#-- TODO: test this as can't really test as insufficient data in it
 #-- script to generate ngis mq reports per GLH
 #-- requires an R version that isn't default on Index (see README.md)
 #-- run with /usr/local/R/3.5.3/bin/Rscript ngis-mq-report.r 
@@ -10,67 +11,114 @@ library(openxlsx)
 
 #-- connect to results db
 drv <- dbDriver("PostgreSQL")
-p <- getprofile("indx_con")
+p <- getprofile(c("indx_con", "cdr_auto"))
 res_db_con <- dbConnect(drv,
              dbname = "metrics",
-             host     = p$host,
-             port     = p$port,
-             user     = p$user,
-             password = p$password)
+             host     = p$indx_con$host,
+             port     = p$indx_con$port,
+             user     = p$indx_con$user,
+             password = p$indx_con$password)
+cdr_db_con <- dbConnect(drv,
+             dbname = "central_data_repo",
+             host     = p$cdr_auto$host,
+             port     = p$cdr_auto$port,
+             user     = p$cdr_auto$user,
+             password = p$cdr_auto$password)
 
-#-- get latest data from results db
-md <- dbGetQuery(res_db_con, "select * from ngis_mq_results.vw_latest_metric_results;")
-tr <- dbGetQuery(res_db_con, "
-	select identifier_hr
-		,identifier_type
-		,case
-			when test_result = true then 'PASS'
-			when test_result = false then 'FAIL'
-			else null
-		end as rule_result
-		,glh_test_id as rule
-		,test_datetime as rule_datetime
-		,glh
-	from ngis_mq_results.vw_latest_test_results
-	where glh_report in ('Y')
-	order by glh_test_id, identifier_hr
-	;")
+#-- get latest data from results db (the DQ report view)
+d <- dbGetQuery(res_db_con, '
+select organisation_id as organisation
+	,referral_id as "Referral ID"
+	,nhs_chi_number as "NHS/CHI Number"
+	,patient_date_of_birth as "Date of Birth"
+	,upper(person_first_name) as "Patient First Name"
+	,upper(person_family_name) as "Patient Surname"
+	,upper(patient_administrative_gender) as "Gender"
+	,glh_test_id as "Test ID"
+	,glh_description as "Test Description"
+	,resolution_guidance as "Resolution Guidance"
+	,includes_csv_data as "Includes Data from CSV"
+	,test_de_datetime as "Failed Rule Datetime"
+	,referral_link as "Referral Link"
+from ngis_mq_results.vw_dq_report_table
+where test_result = false and glh_report = \'Y\'
+;')
 
-#-- get rule descriptions
-rules <- dbGetQuery(res_db_con, "
-	select glh_test_id as rule
-		,glh_description as description
+#-- get rule descriptions for both NGIS and DDF rule failures
+rules <- dbGetQuery(res_db_con, '
+	select glh_test_id as "Test ID"
+		,glh_description as "Test Description"
+		,resolution_guidance as "Resolution Guidance"
+		,includes_csv_data as "Includes Data from CSV"
 	from ngis_mq_results.test_type
-	where glh_report in ('Y')
+	where glh_report in (\'Y\')
 	order by glh_test_id
-	;")
+	;')
+ddf_rules <- read.csv("ddf-rule-descriptions.csv", check.names = F)
+rules <- rbind(rules, ddf_rules)
+
+#-- get DDF rule failures
+ddf_d <- dbGetQuery(cdr_db_con, paste(readLines("ddf-rer-results.sql"), collapse = " "))
+
+#-- get participant details to merge into ddf rule failures
+p_details <- dbGetQuery(res_db_con, paste(readLines("proband-details.sql"), collapse = " "))
+
+#-- merge the two together
+ddf_d <- merge(ddf_d, p_details, by = "Referral ID", all.x = T)
+
+#-- inner merge of the ddf rules data so now only getting rule failures that should be reported to GLH
+ddf_d <- merge(ddf_d, ddf_rules, by = "Test ID", all.y = T)
+
+#-- rbind ddf rule failures onto NGIS MQ rule failures
+d <- rbind(d, ddf_d)
+
+#-- make table of rule failures per organisation and rule
+d_t <- setNames(as.data.frame(table(d$`Test ID`, d$organisation)),
+				 c("Test ID", "organisation", "Number of Failures")
+				 )
+d_t <- merge(d_t, rules, by = "Test ID", all.x = T)
 
 #-- wrapper function to create a worksheet with particular formatting
-write_xlsx <- function(d, fn){
+write_xlsx <- function(t, d, fn){
 	# creates xlsx workbook
-	# header styling
-	r <- rules[rules$rule %in% d$rule,]
+	# header styling and cell styling
 	hs1 <- createStyle(fgFill = "#4F81BD", halign = "LEFT", textDecoration = "Bold", border = "Bottom", fontColour = "white")
 	cs1 <- createStyle(wrapText = TRUE, halign = 'LEFT', valign = 'top')
 	wb <- createWorkbook() 
-	addWorksheet(wb, sheetName='Rule Failures')
-	addWorksheet(wb, sheetName='Rule Descriptions')
+	# add the worksheets
+	addWorksheet(wb, sheetName='Rule Summary')
 	addWorksheet(wb, sheetName='Rule Results')
-	setColWidths(wb, 1, 1:ncol(d), 'auto')
-	setColWidths(wb, 2, 1:ncol(rules), c(15, 100))
+	addWorksheet(wb, sheetName='All Rules')
+	# set the col widths (either manual or auto)
+	setColWidths(wb, 1, 1:ncol(t), c(15, 15, 70, 70, 20))
+	setColWidths(wb, 2, 1:ncol(d), 'auto')
+	setColWidths(wb, 3, 1:ncol(rules), c(15, 70, 70, 20))
+	# freeze the top row of the spreadsheet
 	freezePane(wb, 1, firstRow = TRUE)
 	freezePane(wb, 2, firstRow = TRUE)
-	writeData(wb, 'Rule Failures', d[d$rule_result %in% c('FAIL'), colnames(d) %in% c("identifier_hr", "identifier_type", "rule")], headerStyle = hs1)
-	writeData(wb, 'Rule Descriptions', r, headerStyle = hs1)
-	writeData(wb, 'Rule Results', d)
-	addStyle(wb, 2, style = cs1, rows = 2:(nrow(r) + 1), cols = 1:ncol(r), gridExpand = TRUE)
+	freezePane(wb, 3, firstRow = TRUE)
+	# add in the data and set the headerstyles
+	writeData(wb, 'Rule Summary', t, headerStyle = hs1)
+	writeData(wb, 'Rule Results', d, headerStyle = hs1)
+	writeData(wb, 'All Rules', rules, headerStyle = hs1)
+	# add the cell styles to just 1st and 3rd sheets (where we want wrapping)
+	addStyle(wb, 1, style = cs1, rows = 2:(nrow(rules) + 1), cols = 1:ncol(rules), gridExpand = TRUE)
+	addStyle(wb, 3, style = cs1, rows = 2:(nrow(rules) + 1), cols = 1:ncol(rules), gridExpand = TRUE)
+	# make the hyperlinks and overwrite the relevant data
+	links  <-  d$`Referral Link`
+	names(links) <- rep("Referral Link", length(links))
+	class(links) <- "hyperlink"
+	writeData(wb, 2, x = links, startCol = which(colnames(d) == "Referral Link"), startRow = 2)
+	# write out the workbook
 	saveWorkbook(wb, fn, overwrite = TRUE)
 }
 
 #-- create individual GLH xlsx
-for(glh in unique(md$glh)){
-	fn <- paste0("gmc-dq-results-", glh, '.xlsx')
-	write_xlsx(tr[tr$glh %in% glh, !colnames(tr) %in% c('glh')],fn)
+for(glh in unique(d$organisation)){
+	fn <- paste0("gmc-dq-results-", gsub(".", "-", make.names(glh), fixed = T), '.xlsx')
+	d_glh <- d[d$organisation %in% glh, !colnames(d) %in% c('organisation')]
+	t_glh <- d_t[d_t$organisation %in% glh, !colnames(d_t) %in% c('organisation')]
+	write_xlsx(t_glh, d_glh, fn)
 }
 
 #-- zip up the resulting files
